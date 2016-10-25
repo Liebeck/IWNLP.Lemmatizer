9431245dffe44f6e92b46b12403f00e0e670ab59
Liebeck <Liebeck@users.noreply.github.com>
2016-10-25 11:45:09 +0200

    Move the creation of the lemmatization file into IWNLP.Lemmatizer

---
+++ IWNLP.Lemmatizer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IWNLP.Lemmatizer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 2) 
+            {
+                Lemmatizer lemmatizer = new Lemmatizer();
+                String parsedWiktionary = args[0];
+                String IWNLPExportPath = args[1];
+                lemmatizer.CreateMapping(parsedWiktionary);
+                lemmatizer.Save(IWNLPExportPath);
+            }
+        }
+    }
+}
