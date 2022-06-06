218693b311bbe80dd09e2f9d7354a1e97ab23068
Matthias Liebeck <Liebeck@users.noreply.github.com>
2022-06-07 01:22:05 +0200

    ➖ Remove dependency on NuGet package GenericXMLSerializer

---
+++ IWNLP.Lemmatizer.Models/XMLSerializer.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace IWNLP.Lemmatizer.Models
+{
+    public class XMLSerializer
+    {
+        public static void Serialize<T>(T data, string path) where T : class
+        {
+            Serialize<T>(data, path, "root");
+        }
+
+        public static void Serialize<T>(T data, string path, string xmlRootAttributeName) where T : class
+        {
+            using (FileStream stream = new FileStream(path, FileMode.Create))
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), new XmlRootAttribute(xmlRootAttributeName));
+                xmlSerializer.Serialize(stream, data);
+            }
+        }
+
+        public static T Deserialize<T>(string path, string xmlRootAttributeName) where T : class
+        {
+            using (FileStream stream = new FileStream(path, FileMode.Open))
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), new XmlRootAttribute(xmlRootAttributeName));
+                return xmlSerializer.Deserialize(stream) as T;
+            }
+        }
+
+        public static T Deserialize<T>(string path) where T : class
+        {
+            return Deserialize<T>(path, "root");
+        }        
+    }
+}
